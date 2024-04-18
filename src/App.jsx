75cf951243f003a376809429a9abf029fccf26d1
Aiker Ary Acosta Cantillo <aikiringo26@gmail.com>
2024-04-18 11:15:05 -0500

    First styling for header

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import Header from './components/header'
 import './App.css'
 
 function App() {
@@ -6,6 +7,13 @@ function App() {
 
   return (
     <>
+      <Header />
+      <div className="container">
+        <h2>Counter</h2>
+        <p>{count}</p>
+        <button onClick={() => setCount(count + 1)}>Increment</button>
+        <button onClick={() => setCount(count - 1)}>Decrement</button>
+      </div>
     </>
   )
 }
