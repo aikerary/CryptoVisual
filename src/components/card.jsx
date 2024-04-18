d274fe095eacd9de719f8b2454ec77210264154e
Aiker Ary Acosta Cantillo <aikiringo26@gmail.com>
2024-04-18 11:45:45 -0500

    Setting up the preparations for cchart

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
-import './css/card.css';
+import React from "react";
+import "./css/card.css";
 
 const Card = ({ cryptoName }) => {
+  const equivalencias = {
+    Bitcoin: "BTC",
+    Ethereum: "ETH",
+  };
   return (
     <section className="card">
       <h2>{cryptoName}</h2>
-      {/* Agrega aquí el contenido adicional de la tarjeta */}
     </section>
   );
 };
